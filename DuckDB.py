423badbfc59396eaf009b9b9b3bde2be8ca3dc51
George Fraser <george.w.fraser@gmail.com>
2023-09-25 14:28:35 -0700

    Use temp directory

--- DuckDB.py
+++ DuckDB.py
@@ -3,25 +3,28 @@ import time
 import statistics
 
 # Configuration.
-database = "/mnt/disks/ssd/db"
-# database = "db" # Macbook
-scales = ["63", "125", "250"]
-results = {}
+database = "db"
+temp = "temp"
 
+# Generate data.
+scales = []
 for scale in scales:
-    # Generate data.
     print("Scale {scale} Generate".format(scale=scale))
     generate = """
     install tpcds;
     load tpcds;
+    drop schema if exists sf{scale} cascade;
     create schema sf{scale};
     call dsdgen(sf={scale}, schema=sf{scale});
     """.format(scale=scale)
     connection = duckdb.connect(database=database)
-    # connection.sql(generate)
+    connection.sql(generate)
     connection.close()
 
-    # Query data.
+# Query data.
+scales = ["1000"]
+results = []
+for scale in scales:
     times = []
     exclude = [4, 14, 19, 23, 54, 67, 72, 78, 95]
     for i in range(1, 100):
@@ -29,12 +32,12 @@ for scale in scales:
             continue
         print("Scale {scale} Query {i}".format(scale=scale, i=i))
         connection = duckdb.connect(database=database, read_only=True)
-        connection.sql("set schema = sf{scale}; set enable_progress_bar = true;".format(scale=scale))
+        connection.sql("set schema = 'sf{scale}'; set enable_progress_bar = true; set temp_directory = '{temp}';".format(scale=scale, temp=temp))
         start = time.time()
         connection.sql("pragma tpcds({i});".format(i=i))
         end = time.time()
         times.append(end-start)
         connection.close()
 
-    results[scale] = statistics.geometric_mean(times)
+    results.append(statistics.geometric_mean(times))
     print(results)
